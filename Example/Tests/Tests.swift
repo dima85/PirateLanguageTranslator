34cf5f50c0dfd101722048e5c20a276e42e24d63
Dmytro.Shykaliuk <dmytro.shykaliuk@check24.de>
2016-06-13 17:19:22 +0200

    uncomment test

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -18,27 +18,20 @@ class IntegrationTests: XCTestCase {
     }
     
     func test_when_request_translation_should_succesfully_return_translated_text() {
-//        let expectation = expectationWithDescription("Successfull translation")
-//        
-//        var translatedText: String!
-//        
-//        translator.translate("Hello world!", success: { (translated) in
-//            translatedText = translated
-//            expectation.fulfill()
-//            }) { (error) in
-//                XCTFail("No error should appear")
-//        }
-//        
-//        waitForExpectationsWithTimeout(5.0, handler: nil)
-//        
-//        XCTAssertEqual("Avast world!", translatedText)
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+        let expectation = expectationWithDescription("Successfull translation")
+        
+        var translatedText: String!
+        
+        translator.translate("Hello world!", success: { (translated) in
+            translatedText = translated
+            expectation.fulfill()
+            }) { (error) in
+                XCTFail("No error should appear")
         }
-    }
+        
+        waitForExpectationsWithTimeout(5.0, handler: nil)
+        
+        XCTAssertEqual("Avast world!", translatedText)
+    }    
     
 }
